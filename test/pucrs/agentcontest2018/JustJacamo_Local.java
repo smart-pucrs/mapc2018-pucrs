f3a5c1f456d9296535e91c58a2011ed654a8d89f
TabajaraKrausburg <tabajararodrigues@yahoo.com.br>
2018-09-05 13:56:09 -0300

    files for lab server connection

---
+++ test/pucrs/agentcontest2018/JustJacamo_Local.java
@@ -0,0 +1,29 @@
+package pucrs.agentcontest2018;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import jacamo.infra.JaCaMoLauncher;
+import jason.JasonException;
+import massim.Server;
+
+
+public class JustJacamo_Local {
+
+	@Before
+	public void setUp() {
+
+		try {			
+			JaCaMoLauncher.main(new String[] {"pucrs-mapc_teamB.jcm"});
+		} catch (JasonException e) {
+			System.out.println("Exception: "+e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void run() {
+	}
+
+}
